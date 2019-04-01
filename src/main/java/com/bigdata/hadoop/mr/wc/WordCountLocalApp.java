e8b100fe52627a9675490692eb3509c89ac59b4c
panjizhe <panjizhemr@sina.com>
2019-04-01 10:36:03 +0800

    feat: map reduce实现词频统计 driver实现

--- src/main/java/com/bigdata/hadoop/mr/wc/WordCountLocalApp.java
+++ src/main/java/com/bigdata/hadoop/mr/wc/WordCountLocalApp.java
@@ -14,22 +14,22 @@ import java.net.URISyntaxException;
 /**
  * 通用模板, 配置Mapper Reducer的相关属性
  */
-public class WordCountApp {
+public class WordCountLocalApp {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException {
-        System.setProperty("HADOOP_USER_NAME", "hadoop");
+//        System.setProperty("HADOOP_USER_NAME", "hadoop");
 
         Configuration configuration = new Configuration();
         // 副本系数
-        configuration.set("dfs.replication", "1");
-        // 配置外网访问
-        configuration.set("dfs.client.use.datanode.hostname","true");
-        // 配置文件服务器地址
-        configuration.set("fs.defaultFS", "hdfs://zhege:8020");
+//        configuration.set("dfs.replication", "1");
+//        // 配置外网访问
+//        configuration.set("dfs.client.use.datanode.hostname","true");
+//        // 配置文件服务器地址
+//        configuration.set("fs.defaultFS", "hdfs://zhege:8020");
         // 创建一个Job
         Job job = Job.getInstance(configuration);
         // 设置Job对应的主类
-        job.setJarByClass(WordCountApp.class);
+        job.setJarByClass(WordCountLocalApp.class);
         job.setMapperClass(WordCountMapper.class);
         job.setReducerClass(WordCountReducer.class);
 
@@ -46,8 +46,8 @@ public class WordCountApp {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
-        FileInputFormat.setInputPaths(job, new Path("/wordcount/input"));
-        FileOutputFormat.setOutputPath(job, new Path("/wordcount/output"));
+        FileInputFormat.setInputPaths(job, new Path("input"));
+        FileOutputFormat.setOutputPath(job, new Path("output"));
 
         // 提交job
         boolean result = job.waitForCompletion(true);
